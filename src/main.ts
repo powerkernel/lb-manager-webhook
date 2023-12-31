e8f6752796283610fd2a7608d9996ca12d2b1563
Harry Tang <harry@powerkernel.com>
2023-12-31 23:20:35 +0000

    chore: updates tls path

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import * as fs from 'fs';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('/usr/src/app/tls.key'),
-    cert: fs.readFileSync('/usr/src/app/tls.crt'),
+    key: fs.readFileSync('/usr/src/tls/tls.key'),
+    cert: fs.readFileSync('/usr/src/tls/tls.crt'),
   };
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
