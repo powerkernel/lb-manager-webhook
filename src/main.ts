82cb2e7387e746c2f5b530d88f0392b56bc87681
Harry Tang <harry@powerkernel.com>
2023-12-31 22:58:12 +0000

    feat: enables TLS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/tls.key'),
+    cert: fs.readFileSync('./secrets/tls.crt'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
